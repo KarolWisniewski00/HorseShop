c9e9335a1017a0030fe15b3c3a232b78bafccfc4
Karol <karol.wisniewski2901@gmail.com>
2023-10-24 12:51:06 +0200

    add filter products

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     @include('components/nav-client')
-    
+    @include('components/products-filter')
     @include('components/footer')
     <!--hamburger menu-->
     <script src="{{asset('asset/js/hamburger-menu.js')}}"></script>
