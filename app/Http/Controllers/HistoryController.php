4c9a28bef0fb3d67c5994d5b6619640eade83daa
Karol <karol.wisniewski2901@gmail.com>
2024-02-17 14:28:25 +0100

    change sort

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -22,7 +22,7 @@ class HistoryController extends Controller
             $trail->push('Historia', route('history'));
         });
         $user = Auth::user();
-        $orders = Order::where('user_id', $user->id)->paginate(10);
+        $orders = Order::where('user_id', $user->id)->orderBy('created_at', 'desc')->paginate(10);
         $status = [
             'PENDING' => OrderStatus::PENDING,
             'PROGRESS' => OrderStatus::PROGRESS,
