b61da29f5148a87eb40f80e541f06e132cb7a399
Karol <karol.wisniewski2901@gmail.com>
2024-02-22 07:44:10 +0100

    update servises

--- config/services.php
+++ config/services.php
@@ -33,7 +33,7 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'https://suplementyhh.pl/login/google/callback',
+        'redirect' => 'https://suplementyhealthyhorse.pl/login/google/callback',
     ],
     
 ];
